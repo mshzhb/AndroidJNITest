c8474fcda9d2686c2571f253eb94bad7f9ae2abb
Tong Liu <mshzhb@gmail.com>
2017-12-03 16:53:46 -0500

    first commit

---
+++ app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,52 @@
+#include <jni.h>
+#include <string>
+#include <time.h>
+#include <stdio.h>
+#include <math.h>
+#include <sys/time.h>
+
+long PRIME_MAX = 10000;
+int my_is_prime(long a);
+
+extern "C"
+JNIEXPORT jstring
+
+JNICALL
+Java_com_androidtest_mshzhb_androidtest_MainActivity_stringFromJNI(
+        JNIEnv *env,
+        jobject /* this */) {
+    std::string hello = "Hello from C++";
+    return env->NewStringUTF(hello.c_str());
+}
+
+
+extern "C"
+JNIEXPORT jlong
+
+JNICALL
+Java_com_androidtest_mshzhb_androidtest_MainActivity_primeTestCPPhelper(
+        JNIEnv *env,
+jobject /* this */
+) {
+    long num;
+    for (num = 0; num < PRIME_MAX; num++)
+        my_is_prime(num);
+
+    return 0;
+
+}
+
+int my_is_prime(long a)
+{
+    long n;
+    if(a == 2){
+        return 1;
+    }else if(a <= 1 || a % 2 == 0){
+        return 0;
+    }
+    long max = sqrt(a);
+    for( n= 3; n <= max; n+= 2){
+        if(a % n == 0){ return 0; }
+    }
+    return 1;
+}
---
+++ app/src/main/java/com/androidtest/mshzhb/androidtest/MainActivity.java
@@ -0,0 +1,86 @@
+package com.androidtest.mshzhb.androidtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    public long PRIME_MAX = 10000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        final TextView textViewPrimeTest = (TextView) findViewById(R.id.textViewPrime);
+        Button buttonPrimeTest = (Button) findViewById(R.id.buttonPrime);
+
+        buttonPrimeTest.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                long primeTimeJava = primeTestJava();
+                long primeTimeCPP = primeTestCPP();
+
+                Double speedup = BigDecimal.valueOf((double)primeTimeJava/primeTimeCPP*100)
+                        .setScale(2, RoundingMode.HALF_UP)
+                        .doubleValue();
+
+                textViewPrimeTest.setText("Java time: " + primeTimeJava + " milliseconds\n" +
+                        "C++ time: "+ primeTimeCPP + " milliseconds\n" +
+                "Speedup: " + speedup +"%");
+            }
+        });
+    }
+
+    public long primeTestCPP(){
+        long startTime = System.currentTimeMillis();
+        primeTestCPPhelper();
+        long stopTime = System.currentTimeMillis();
+        long elapsedTime = stopTime - startTime;
+        return elapsedTime;
+    }
+
+    public long primeTestJava(){
+        long number;
+        long startTime = System.currentTimeMillis();
+
+        for (number=0; number<PRIME_MAX; number++)
+            isprime(number);
+
+        long stopTime = System.currentTimeMillis();
+        long elapsedTime = stopTime - startTime;
+        return elapsedTime;
+    }
+
+    public boolean isprime(long a)
+    {
+        if(a == 2){
+            return true;
+        }else if(a <= 1 || a % 2 == 0){
+            return false;
+        }
+        long max = (long)Math.sqrt(a);
+        for(long n= 3; n <= max; n+= 2){
+            if(a % n == 0){ return false; }
+        }
+        return true;
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+    public native long primeTestCPPhelper();
+}
